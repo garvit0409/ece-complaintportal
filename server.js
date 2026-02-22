844211a8d72081b2a141d4f8cc224955dc1f6384
Garvit Arora <garvitarora2310@gmail.com>
2026-02-22 13:54:41 +0530

    Update port for deployment

--- server.js
+++ server.js
@@ -74,7 +74,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 // --- API ROUTES ---
