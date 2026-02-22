3a5c3e454d26bcac9e43b1f3df5c5e6843815db8
Garvit Arora <garvitarora2310@gmail.com>
2026-02-22 13:39:55 +0530

    Update port for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
